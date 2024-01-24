50b59dc7a99d920f8265bc41975be9f50e084985
Erik Skopp <129506395+eskopp@users.noreply.github.com>
2024-01-24 20:43:15 +0100

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	result := Add(5, 3)
-	expected := 8
+	result := Add(Add(5, 3),Add(2,1))
+	expected := 11
 
 	if result != expected {
 		t.Errorf("Add(5, 3) = %d; want %d", result, expected)
