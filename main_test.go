0a36f7a30f08e5f28cdf48955cfb599e475b8a53
eskopp <skopp.erik@gmail.com>
2024-01-24 17:38:22 +0100

    Create Error in Workflow

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	result := Add(5, 3)
-	expected := 8
+	expected := 9
 
 	if result != expected {
 		t.Errorf("Add(5, 3) = %d; want %d", result, expected)
