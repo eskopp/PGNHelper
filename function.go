07f190c433a4e907aece31971f7ca0786b91c30a
Erik Skopp <skopp.erik@gmail.com>
2024-02-12 01:34:51 +0100

    Parse Function

--- function.go
+++ function.go
@@ -16,15 +16,59 @@ func pgnHelp() {
 }
 
 func noflag() {
-	color.Red("try pgn --help ?")
+	color.Red("try pgn -help ?")
 }
 
 func parsepgn() {
+	var inputpgn string
+	var outjson string
+
 	fmt.Println("Parsing PGN-Datei:")
+	if len(os.Args) < 4 {
+		color.Red("No Args")
+		os.Exit(1)
+	} else {
+		inputpgn = os.Args[2]
+		outjson = os.Args[3]
+	}
+
+	// Prüft, ob die Inputdatei vorhanden ist
+	fmt.Println(inputpgn)
+	if _, err := os.Stat(inputpgn); os.IsNotExist(err) {
+		color.Red(inputpgn + " File not exist")
+		os.Exit(1)
+	}
+
+	// Hier findet die eigentliche Verarbeitung statt
+	if err := parsePGNFile(inputpgn, outjson); err != nil {
+		panic(err)
+	}
 }
 
 func parsejson() {
-	fmt.Println("Parsing JSON-Datei:")
+	var inputjson string
+	var outpgn string
+
+	fmt.Println("Parsing PGN-Datei:")
+	if len(os.Args) < 4 {
+		color.Red("No Args")
+		os.Exit(1)
+	} else {
+		inputjson = os.Args[2]
+		outpgn = os.Args[3]
+	}
+
+	// Prüft, ob die Inputdatei vorhanden ist
+	fmt.Println(inputjson)
+	if _, err := os.Stat(inputjson); os.IsNotExist(err) {
+		color.Red(inputjson + " File not exist")
+		os.Exit(1)
+	}
+
+	// Magic
+	if err := parseJSONFile(inputjson, outpgn); err != nil {
+		panic(err)
+	}
 }
 
 func unknownflag() {
